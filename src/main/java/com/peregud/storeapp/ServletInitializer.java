edd62d8fe42310e7f16cfb658d51730600a8ea9b
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-10-17 10:09:55 +0300

    initial commit

---
+++ src/main/java/com/peregud/storeapp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.peregud.storeapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StoreAppApplication.class);
+    }
+
+}
---
+++ src/test/java/com/peregud/storeapp/StoreAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.peregud.storeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
